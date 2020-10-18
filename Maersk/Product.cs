999713cc6b1e21d717cb67117f55514e3f4d60ca
kanikachander <40595751+kanikachander@users.noreply.github.com>
2020-10-18 14:33:22 +0530

    Create Product.cs

---
+++ Maersk/Product.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Maersk
+{
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+    }
+}
